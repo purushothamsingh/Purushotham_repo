b5770c21e78601f4aa9e657419ed89b19991812f
purushotham_singh <kpurushothamsingh@gmail.com>
2022-07-02 00:35:22 +0530

    added animation

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,12 +4,14 @@ for (var i =0; i< no_of_drum;i++){
   // alert("You Clicked Letter is..!"+ this.innerHTML);
   var key = this.innerHTML;
   palySound(key);
+  addAnimation(key)
    
    })
 }
 
 document.addEventListener("keypress",function (event) {
     palySound(event.key);
+    addAnimation(event.key);
 })
 function palySound(key){
     switch (key) {
@@ -43,9 +45,17 @@ function palySound(key){
             var tom1  = new Audio("sounds/tom-4.mp3");
             tom1.play();
             break;
-    
-    
         default:
             break;
        }
+}
+
+function addAnimation (key) {
+    var pressedkey = document.querySelector("."+key);
+    pressedkey.classList.add("pressed");
+    setTimeout(timestop ,200,pressedkey);
+
+}
+function timestop(mypressed) {
+    mypressed.classList.remove("pressed");
 }
